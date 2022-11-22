7221a819cc8f0ca7fe55a6b0113ccf106c57b19e
emfernandezp <emfernandezp@indra.es>
2022-11-22 16:01:48 +0100

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  heroeBorrado: string = '';
+  public borrarHeroe() {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+}
